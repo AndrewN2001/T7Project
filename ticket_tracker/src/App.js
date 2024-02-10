3118cc395c808385fb3c3025e1bb4e1440c43b69
codea-n <alihamza5399@gmail.com>
2024-02-10 15:32:53 -0600

    navbar updates

--- ticket_tracker/src/App.js
+++ ticket_tracker/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import "./App.css"
 import FlightForm from './components/Searchbar';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div class="">
-      <FlightForm/>
+      <Navbar/>
     </div>
   );
 }
---
+++ ticket_tracker/src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Navbar(){
+    return(
+        <div>
+            <nav class="flex justify-center">
+                <a class="px-2 py-2 text-gray-600 hover:bg-teal-200">Home Page</a>
+                <a class="px-2 py-2 text-gray-600 hover:bg-teal-200">Search for Flights</a>
+</nav>
+        </div>
+    )
+}
