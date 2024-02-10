9b6de98648984b77abe49d52b83e6a13917a189f
codea-n <alihamza5399@gmail.com>
2024-02-10 16:39:50 -0600

    commit

--- ticket_tracker/src/components/Navbar.js
+++ ticket_tracker/src/components/Navbar.js
@@ -3,9 +3,9 @@ import React from "react";
 export default function Navbar(){
     return(
         <div>
-            <nav class="flex justify-center">
-                <a class="px-2 py-2 text-gray-600 hover:bg-teal-200">Home Page</a>
-                <a class="px-2 py-2 text-gray-600 hover:bg-teal-200">Search for Flights</a>
+            <nav class="flex justify-center focus:border-blue border-8 border-solid">
+                <a class="px-2 py-2 text-black hover:bg-blue-300 active:bg-blue-300">Home Page</a>
+                <a class="px-2 py-2 text-black hover:bg-blue-300 active:bg-blue-300" >Search For Flights</a>
 </nav>
         </div>
     )
