55c1070afdf4ff78e3ebcce9665d3069b0588835
ANDREW NGUYEN <andrew.nguyen.ta@gmail.com>
2024-02-11 02:21:31 -0600

    citySearch api

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const Amadeus = require('amadeus');
@@ -13,16 +14,33 @@ app.get('/api/data', (req, res) => {
         destinationLocationCode: 'BKK',
         departureDate: '2024-06-01',
         adults: '2'
-    }).then(function(response){
+    }).then(function (response) {
         // Send the response from Amadeus API back to the client
         res.json(response.data);
-    }).catch(function(responseError){
+    }).catch(function (responseError) {
         // Handle errors if any
         console.log(responseError.code);
         res.status(500).json({ error: 'Internal Server Error' });
     });
 });
 
-app.listen(4000, () =>{
+app.get('/api/citydata', (req, res) => {
+    const amadeus = new Amadeus({
+        clientId: process.env.AMADEUS_CLIENT_ID,
+        clientSecret: process.env.AMADEUS_CLIENT_SECRET
+    });
+    const keyword = req.query.city || "Paris";
+    amadeus.referenceData.location.get.citySearch({
+        keyword
+    })
+        .then(function (response) {
+            res.json(response.data)
+        }).catch(function (response) {
+            console.error(response);
+        })
+})
+
+
+app.listen(4000, () => {
     console.log("Listening on port 4000");
 });
--- ticket_tracker/src/components/Searchbar.js
+++ ticket_tracker/src/components/Searchbar.js
@@ -27,27 +27,6 @@ export default function FlightForm(){
             <div className="w-min h-min border-2 border-gray-400 rounded-md flex justify-center items-center">
                 <form className="flex flex-col gap-3 p-3">
                     <textarea type="text" id="depart" rows="8" className="border-2 rounded-md px-2 text-black pt-2 items-center left-4 align-text-top resize-none" placeholder="Going to:"></textarea>
-                    <div className="flex gap-1">
-                        <input type="date" id="depart_date" name="depart_date" className="border-2 px-2 rounded-md" placeholder="Depart Date:"></input>
-                        <input type="date" id="return_date" name="return_date" className="border-2 px-2 rounded-md" placeholder="Return Date:"></input>
-                        <div className="h-fit">
-                            <button id="passenger" type="button" className="bg-white w-[224.67px] py-2 border-2 rounded-md whitespace-nowrap" onClick={TravelerSelect}>
-                                {adults > 0 ? `${adults} Adults` : "Traveler Information"}
-                            </button>
-                            <div id="passenger_select" className={`bg-white border-2 absolute mt-1 rounded-md z-20 px-3 w-[224.67px] ${selection ? "visible" : "invisible"}`}>
-                                <div className="flex items-center gap-3 py-3 justify-between" >
-                                    <p1>Adults</p1>
-                                    <div className="flex gap-3 justify-center items-center">
-                                        <button type="button" className="w-fit h-fit p-1 rounded-full border-2 border-black bg-white hover:bg-slate-200" onClick={DecrementAdults}><FaMinus/></button>
-                                        <div>{adults}</div>
-                                        <button type="button" className="w-fit h-fit p-1 rounded-full border-2 border-black bg-white hover:bg-slate-200" onClick={IncrementAdults}><FaPlus/></button>
-                                    </div>
-                                </div>
-
-                                <button type="button" className="bg-cyan-500 hover:bg-cyan-600 w-full py-1 mb-2 rounded-md" onClick={TravelerSelect}>Apply</button>
-                            </div>
-                        </div>
-                    </div>
                     <input type="submit" value="Submit" className="bg-cyan-500 hover:bg-cyan-600 p-2 rounded-md"/>
                 </form>
             </div>  
