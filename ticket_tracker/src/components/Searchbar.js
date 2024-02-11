dc580427b1368342f6e127810b2929016fae1777
ANDREW NGUYEN <andrew.nguyen.ta@gmail.com>
2024-02-11 12:14:54 -0600

    updated UI

--- ticket_tracker/src/components/Searchbar.js
+++ ticket_tracker/src/components/Searchbar.js
@@ -3,9 +3,6 @@ import React, {useState} from "react";
 export default function FlightForm(){
     const [data, setData] = useState("")
     const [content, setContent] = useState("")
-    const [adults, setAdults] = useState(0);
-    const [children, setChildren] = useState(0);
-    const [selection, setSelection] = useState(false);
 
     const sendData = async (e) => {
         e.preventDefault()
@@ -28,32 +25,16 @@ export default function FlightForm(){
     };
     
     return(
-        <div className="min-h-screen min-w-screen flex justify-center items-center">
-            <div className="w-min h-min border-2 border-gray-400 rounded-md flex flex-col justify-center items-center">
+        <div className="min-h-screen min-w-screen flex flex-col justify-center items-center bg-slate-300">
+            <div className="text-center pb-5">
+                <h1 className="text-7xl text-gray-800">Welcome to <span className="text-cyan-500">Runaway.</span></h1>
+                <h1 className="pt-4 text-xl text-gray-500">For whenever you want to hide from your wife (or husband)</h1>
+            </div>
+
+            <div className="w-min h-min rounded-sm flex flex-col justify-center items-center bg-slate-100">
                 <form className="flex flex-col gap-3 p-3" onSubmit={sendData}>
                     <textarea value = {content} onChange={e => setContent(e.target.value)} type="text" id="depart" rows="8" cols="60" className="border-2 rounded-md px-2 text-black pt-2 items-center left-4 align-text-top resize-none" placeholder="Going to:"></textarea>
                     {console.log(content)}
-                    {/* <div className="flex gap-1">
-                        <input type="date" id="depart_date" name="depart_date" className="border-2 px-2 rounded-md" placeholder="Depart Date:"></input>
-                        <input type="date" id="return_date" name="return_date" className="border-2 px-2 rounded-md" placeholder="Return Date:"></input>
-                        <div className="h-fit">
-                            <button id="passenger" type="button" className="bg-white w-[224.67px] py-2 border-2 rounded-md whitespace-nowrap" onClick={TravelerSelect}>
-                                {adults > 0 ? `${adults} Adults` : "Traveler Information"}
-                            </button>
-                            <div id="passenger_select" className={`bg-white border-2 absolute mt-1 rounded-md z-20 px-3 w-[224.67px] ${selection ? "visible" : "invisible"}`}>
-                                <div className="flex items-center gap-3 py-3 justify-between" >
-                                    <h1>Adults</h1>
-                                    <div className="flex gap-3 justify-center items-center">
-                                        <button type="button" className="w-fit h-fit p-1 rounded-full border-2 border-black bg-white hover:bg-slate-200" onClick={DecrementAdults}><FaMinus/></button>
-                                        <div>{adults}</div>
-                                        <button type="button" className="w-fit h-fit p-1 rounded-full border-2 border-black bg-white hover:bg-slate-200" onClick={IncrementAdults}><FaPlus/></button>
-                                    </div>
-                                </div>
-
-                                <button type="button" className="bg-cyan-500 hover:bg-cyan-600 w-full py-1 mb-2 rounded-md" onClick={TravelerSelect}>Apply</button>
-                            </div>
-                        </div>
-                    </div> */}
                     <input type="submit" value="Submit" className="bg-cyan-500 hover:bg-cyan-600 p-2 rounded-md"/>
                 </form>
                 <div className={`pb-4 ${data != "" ? `visible` : `invisible`}`}>{`Total cost of the plane ticket: ${data.finalCost}`}</div>
