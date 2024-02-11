2e1b4dd44f16ef0ab2ecbb37969d69ad04074748
Osaid Mohammad <73810127+OsaCode468@users.noreply.github.com>
2024-02-11 00:09:10 -0600

    created post request

--- ticket_tracker/src/App.js
+++ ticket_tracker/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import "./App.css"
+import Logo from './Logo';
 import FlightForm from './components/Searchbar';
-
 function App() {
   return (
     <div>
+      <Logo/>
       <FlightForm/>
     </div>
   );
--- ticket_tracker/src/components/Searchbar.js
+++ ticket_tracker/src/components/Searchbar.js
@@ -2,10 +2,28 @@ import React, {useState} from "react";
 import { FaPlus, FaMinus } from "react-icons/fa6";
 
 export default function FlightForm(){
+    const [data, setData] = useState(null)
+    const [content, setContent] = useState("")
     const [adults, setAdults] = useState(0);
     const [children, setChildren] = useState(0);
     const [selection, setSelection] = useState(false);
-
+    const sendData = async (e) => {
+        e.preventDefault()
+        try {
+            const response = await fetch("http://127.0.0.1:4000", {
+                method: "POST",
+                body: JSON.stringify({ key: content }),
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            });
+            const responseData = await response.json(); // Extracting JSON data from response
+            setData(responseData); // Setting data state with the extracted data
+            console.log(responseData); // Logging the data for verification
+        } catch (error) {
+            console.error("Error:", error);
+        }
+    };
     const TravelerSelect = () => {
         setSelection(!selection);
     }
@@ -25,8 +43,9 @@ export default function FlightForm(){
     return(
         <div className="min-h-screen min-w-screen flex justify-center items-center">
             <div className="w-min h-min border-2 border-gray-400 rounded-md flex justify-center items-center">
-                <form className="flex flex-col gap-3 p-3">
-                    <textarea type="text" id="depart" rows="8" className="border-2 rounded-md px-2 text-black pt-2 items-center left-4 align-text-top resize-none" placeholder="Going to:"></textarea>
+                <form className="flex flex-col gap-3 p-3" onSubmit={sendData}>
+                    <textarea value = {content} onChange={e => setContent(e.target.value)} type="text" id="depart" rows="8" className="border-2 rounded-md px-2 text-black pt-2 items-center left-4 align-text-top resize-none" placeholder="Going to:"></textarea>
+                    {console.log(content)}
                     <div className="flex gap-1">
                         <input type="date" id="depart_date" name="depart_date" className="border-2 px-2 rounded-md" placeholder="Depart Date:"></input>
                         <input type="date" id="return_date" name="return_date" className="border-2 px-2 rounded-md" placeholder="Return Date:"></input>
@@ -36,7 +55,7 @@ export default function FlightForm(){
                             </button>
                             <div id="passenger_select" className={`bg-white border-2 absolute mt-1 rounded-md z-20 px-3 w-[224.67px] ${selection ? "visible" : "invisible"}`}>
                                 <div className="flex items-center gap-3 py-3 justify-between" >
-                                    <p1>Adults</p1>
+                                    <h1>Adults</h1>
                                     <div className="flex gap-3 justify-center items-center">
                                         <button type="button" className="w-fit h-fit p-1 rounded-full border-2 border-black bg-white hover:bg-slate-200" onClick={DecrementAdults}><FaMinus/></button>
                                         <div>{adults}</div>
